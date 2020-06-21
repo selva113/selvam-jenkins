1cb423fa2f823aa162ca35a02a8dc361d3c91dfb
selva113 <60591870+selva113@users.noreply.github.com>
2020-06-21 14:13:30 +0530

    Add files via upload
    donedone

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package jenkins.test;
+
+public class Hello {
+
+	private void world() {
+	System.out.println("hello world");
+}
+	
+	public static void main(String[] args) {
+		Hello gh = new Hello();
+		gh.world();
+
+	}
+
+}
